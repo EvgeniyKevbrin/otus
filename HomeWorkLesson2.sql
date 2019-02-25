0abc160cbca3ac91af4c81e53b4c39672451df7a
Evgeniy <evgeniy.kevbrin@yandex.ru>
2019-02-25 13:22:07 -0800

    Lesson 2 Task 1

---
+++ HomeWorkLesson2.sql
@@ -0,0 +1,17 @@
+﻿--Все товары, в которых в название есть пометка urgent или название начинается с Animal
+SELECT [InvoiceLineID]
+      ,[InvoiceID]
+      ,[StockItemID]
+      ,[Description]
+      ,[PackageTypeID]
+      ,[Quantity]
+      ,[UnitPrice]
+      ,[TaxRate]
+      ,[TaxAmount]
+      ,[LineProfit]
+      ,[ExtendedPrice]
+      ,[LastEditedBy]
+      ,[LastEditedWhen]
+  FROM [WideWorldImporters].[Sales].[InvoiceLines]
+  WHERE    (Description LIKE 'Animal%') 
+        OR (Description LIKE '%urgent%')
