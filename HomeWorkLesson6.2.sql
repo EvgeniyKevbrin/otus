4a1cecf23a006a3f1296680a9694a033645dbe30
Evgeniy <evgeniy.kevbrin@yandex.ru>
2019-02-28 13:03:31 -0800

    Lesson 6 task 2,3

---
+++ HomeWorkLesson6.2.sql
@@ -0,0 +1,30 @@
+﻿--2. Вывести список 2х самых популярных продуктов (по кол-ву проданных) в каждом месяце за 2016й год (по 2 самых популярных продукта в каждом месяце)
+
+;WITH cte AS (
+	SELECT s.StockItemName
+		  ,t.TransactionOccurredWhen
+		  ,ROW_NUMBER() OVER (PARTITION BY DATEADD(mm, DATEDIFF(month, 0, TransactionOccurredWhen),0), StockItemName ORDER BY TransactionOccurredWhen) R_N
+	FROM   [Warehouse].[StockItems] s
+		   INNER JOIN [Warehouse].[StockItemTransactions] t ON t.StockItemID = s.StockItemID
+	WHERE t.TransactionOccurredWhen BETWEEN '2016-01-01' AND '2017-01-01' 
+),  item_count_by_month  AS
+(
+	SELECT *
+	      ,MAX(R_N) OVER (PARTITION BY DATEADD(mm, DATEDIFF(month, 0, TransactionOccurredWhen),0), StockItemName) AS MaxItemMonthValue
+	      ,DATEADD(mm, DATEDIFF(month, 0, TransactionOccurredWhen),0) AS [Month]
+	FROM cte
+ ), count_items_without_duplicates AS
+ (
+	SELECT DISTINCT StockItemName,MaxItemMonthValue, [Month]
+	FROM   item_count_by_month
+ ), item_ordering AS
+ (
+	SELECT DISTINCT StockItemName,MaxItemMonthValue, [Month]
+		  ,ROW_NUMBER() OVER (PARTITION BY [Month] ORDER BY MaxItemMonthValue DESC) AS R_N
+	FROM count_items_without_duplicates
+
+ )
+ SELECT StockItemName, MaxItemMonthValue, [Month], R_N
+ FROM   item_ordering
+ WHERE  R_N <=2
+ ORDER BY [Month]
---
+++ HomeWorkLesson6.3.sql
@@ -0,0 +1,27 @@
+﻿	  --Посчитайте по таблице товаров, в вывод также должен попасть ид товара, название, брэнд и цена
+	SELECT  s.StockItemID
+		   ,s.StockItemName
+		   ,ct.TransactionAmount
+		   ,s.Brand
+		   --пронумеруйте записи по названию товара, так чтобы при изменении буквы алфавита нумерация начиналась заново
+		   ,ROW_NUMBER() OVER (PARTITION BY SUBSTRING(s.StockItemName,1,1) ORDER BY  s.StockItemName  ) AS ItemsRowNumberByLetters
+		   --посчитайте общее количество товаров и выведете полем в этом же запросе
+		   ,COUNT(StockItemName) OVER () AS TotalQuantityOfGoods
+		   --посчитайте общее количество товаров в зависимости от первой буквы названия товара
+		   ,COUNT(StockItemName) OVER ( PARTITION BY SUBSTRING(s.StockItemName,1,1) ) AS TotalQuantityOfGoodsByFirstLetter
+		   --отобразите следующий id товара исходя из того, что порядок отображения товаров по имени 
+		   ,LEAD (s.StockItemID) OVER (ORDER BY SUBSTRING(s.StockItemName,1,1) ) NextItem
+		   --предыдущий ид товара с тем же порядком отображения (по имени)
+		   ,LAG (s.StockItemID) OVER (ORDER BY SUBSTRING(s.StockItemName,1,1) ) PreviousItem
+		   --названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"
+		   /*Не получается в одном запросе вывести "No items", в случае если предыдущей строки нет
+		   Ошибка: Conversion failed when converting the varchar value 'No Items' to data type int.
+		   Можно вывести 'No Items' используя CTE. Есть ли способ вывести в одном запросе вывести "No items"?  */
+		   ,LAG (s.StockItemID,2, 0) OVER (ORDER BY SUBSTRING(s.StockItemName,1,1)) TwoRowsBack
+		   --сформируйте 30 групп товаров по полю вес товара на 1 шт
+		   ,NTILE(30) OVER (ORDER BY s.TypicalWeightPerUnit) AS ItemGroups
+	FROM   [Warehouse].[StockItems] s
+				INNER JOIN [Warehouse].[StockItemTransactions] t ON t.StockItemID = s.StockItemID
+				INNER JOIN [Sales].[CustomerTransactions] ct  ON    t.InvoiceID = ct.InvoiceID
+
+
