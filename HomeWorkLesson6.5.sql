83a8f589ed1a89294ea4745556a14f6b74c241bb
Evgeniy <evgeniy.kevbrin@yandex.ru>
2019-03-02 11:00:13 -0800

    HomeWorkLesson6.5

---
+++ HomeWorkLesson6.5.sql
@@ -0,0 +1,17 @@
+﻿-- Выберите по каждому клиенту 2 самых дорогих товара, которые он покупал
+-- В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки
+;WITH cte AS 
+(
+	SELECT c.CustomerId
+		  ,c.CustomerName
+		  ,i.InvoiceID
+		  ,TransactionAmount
+		  ,i.InvoiceDate
+		  ,ROW_NUMBER() OVER (PARTITION BY c.CustomerName ORDER BY TransactionAmount DESC) R_N
+	FROM   [Sales].[Invoices] i
+		   INNER JOIN [Sales].[CustomerTransactions] t ON i.InvoiceID = t.InvoiceID
+		   INNER JOIN [Sales].[Customers] c ON c.CustomerID = t.CustomerID
+)
+SELECT *
+FROM   cte
+WHERE  R_N <= 2
