88441e7b89dbda8b343b145dd4b49e92b606fa90
Evgeniy <evgeniy.kevbrin@yandex.ru>
2019-03-03 06:04:36 -0800

    Lesson7.2.sql

---
+++ Lesson7/Lesson7.2.sql
@@ -0,0 +1,32 @@
+﻿DECLARE @DirectReports TABLE  
+(  
+EmployeeID smallint NOT NULL,  
+[Name] nvarchar(70)  NOT NULL,  
+Title nvarchar(50) NOT NULL,   
+EmployeeLevel int NULL
+); 
+
+
+;WITH DirectReports(EmployeeID, Name, Title, EmployeeLevel) AS   
+(  
+    SELECT EmployeeID, CONCAT(FirstName, LastName), Title, 0 AS EmployeeLevel  
+    FROM dbo.MyEmployees   
+    WHERE ManagerID IS NULL  
+    UNION ALL  
+    SELECT e.EmployeeID, CONCAT(FirstName, LastName) AS Name, e.Title, EmployeeLevel + 1  
+    FROM dbo.MyEmployees AS e  
+        INNER JOIN DirectReports AS d  
+        ON e.ManagerID = d.EmployeeID   
+)
+INSERT INTO @DirectReports
+SELECT EmployeeID, Name, Title, EmployeeLevel
+FROM DirectReports  
+ORDER BY EmployeeID;  
+
+-- план выполнения запроса одинаковый и в случае с временной таблицей и в случае с табличной переменной
+SELECT EmployeeID, CONCAT( REPLICATE(' | ', EmployeeLevel), Name) AS Name, Title, EmployeeLevel
+FROM  @DirectReports
+
+SELECT EmployeeID, CONCAT( REPLICATE(' | ', EmployeeLevel), Name) AS Name, Title, EmployeeLevel
+FROM  #DirectReports
+
