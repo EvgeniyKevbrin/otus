a5b1031bd02666d68a714c777f9510214b89dc85
Evgeniy <evgeniy.kevbrin@yandex.ru>
2019-03-24 22:06:11 +0300

    Corrected HomeWorkLesson6.5

--- Lesson6/HomeWorkLesson6.5.sql
+++ Lesson6/HomeWorkLesson6.5.sql
@@ -2,16 +2,19 @@
 -- В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки
 ;WITH cte AS 
 (
-	SELECT c.CustomerId
-		  ,c.CustomerName
+	SELECT i.CustomerId
 		  ,i.InvoiceID
 		  ,TransactionAmount
 		  ,i.InvoiceDate
-		  ,ROW_NUMBER() OVER (PARTITION BY c.CustomerName ORDER BY TransactionAmount DESC) R_N
+		  ,ROW_NUMBER() OVER (PARTITION BY i.CustomerId ORDER BY TransactionAmount DESC) R_N
 	FROM   [Sales].[Invoices] i
 		   INNER JOIN [Sales].[CustomerTransactions] t ON i.InvoiceID = t.InvoiceID
-		   INNER JOIN [Sales].[Customers] c ON c.CustomerID = t.CustomerID
 )
-SELECT *
+SELECT cte.CustomerId
+	  ,cte.InvoiceID
+	  ,cte.TransactionAmount
+	  ,cte.InvoiceDate
+	  ,c.CustomerName
 FROM   cte
+	   INNER JOIN [Sales].[Customers] c ON c.CustomerID = cte.CustomerID
 WHERE  R_N <= 2
